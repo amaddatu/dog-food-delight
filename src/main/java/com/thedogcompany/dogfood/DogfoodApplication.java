79f00fcc64da82950172bd12c187463c80088636
Anthony Maddatu <amaddatu@gmail.com>
2019-05-15 14:35:27 -0400

    Initialize Spring Boot Repository

---
+++ src/main/java/com/thedogcompany/dogfood/DogfoodApplication.java
@@ -0,0 +1,13 @@
+package com.thedogcompany.dogfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogfoodApplication.class, args);
+    }
+
+}
